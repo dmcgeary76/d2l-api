ca09c41bd670c42bbd722905501b7c7c806f7395
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2023-07-10 16:32:33 -0500

    Update views.py
    revise token method to pass auth url as part of the token request

--- auto/views.py
+++ auto/views.py
@@ -24,16 +24,16 @@ grant_type = 'client_credentials'
 curl_text = 'https://auth.brightspace.com/oauth2/auth?client_id=370b8ba9-a004-4950-aa27-8af6b8ef25e5&client_sceret=yoxhApjjthpOZMq9vOW2hsc4WKsXpK5wArqaCrFYcsE&scope=core:*:*&redirect_uri=https://localhost:8000&grant_type=client_credentials&response_type=code'
 
 def get_token():
-    response = requests.post(curl_text)
-    print(response)
-
     # Create the OAuth object
     oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
     
     # Get the auth url and state
     authorization_url, state = oauth.authorization_url("https://auth.brightspace.com/oauth2/auth")
     print(authorization_url)
-
+    
+    response = requests.post(authorization_url)
+    print(response)
+    
     authorization_code = 'ac.us-east-1.czelmLbPSmoq2xnzZDm56Ot8kLxW080uuYa7ty5qFNU&state=CrqqcTFCI3JKk3WDtZwzUyX0AOvflK'
     
     token = oauth.fetch_token(
